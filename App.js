b0fe44d5255ca98d3c1abf10d50e860939927fb4
Ahmed Shabana <shabana@Ahmeds-MacBook-Pro.local>
2022-03-08 14:44:10 +0200

    just for test

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Button, Text, View } from "react-native";
+import SmsListener from "react-native-android-sms-listener";
+
+import * as Cequens from "./lib/cequens";
 
 export default function App() {
+  console.log("Starting the Appliation");
+  const [inprogress, runSMS] = React.useState(false);
+  const [msgID, setMsgID] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{ inprogress ? "Waiting SMS at device inbox" : "Start OTP Testing for single SMS" } </Text>
+      <Button
+        onPress={async () => {
+          runSMS(true);
+          const msgid = await Cequens.sendSMSAsync(
+            "Cequens",
+            "201003325373",
+            "Text text shabana",
+            Cequens.CeqSupToken
+          );
+          setMsgID(msgid);
+          console.log(msgid);
+          console.log("Start Requesting Permissions");
+          await Cequens.getPermissionReceiveSMS();
+          await Cequens.getPermissionReadSMS();
+          console.log("End Requesting Permissions");
+          SmsListener.addListener((message) => {
+            console.log("Getting a new msg");
+            console.info(message);
+            runSMS(false);
+          });
+          
+        }}
+        title="Start"
+        color="#841584"
+        disabled={inprogress}
+      />
+      <Text>{msgID}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
